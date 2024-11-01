306690c863e2905397884682253cd6045d89c469
sreerampradeep553 <sreerampradeep0553@gmail.com>
2024-11-01 07:47:51 +0530

    task added by pradeep

---
+++ 25102024.py
@@ -0,0 +1,6 @@
+# PACKAGES :- (FOLDER OR DIRECTORY)
+# --> Collection of nodules is called packages
+# --> Collection of variables,class,functions is called modules
+# --> Function is nothing but Block of reusable code.
+
+# PACKAGE --> MODULE --> FUNCTION --> CLASS --> OBJECT
