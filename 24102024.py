e11b438b2120c7564a164b3e7d0ba0e30da54df4
sreerampradeep553 <sreerampradeep0553@gmail.com>
2024-11-01 07:39:00 +0530

    task added by pradeep

---
+++ 24102024.py
@@ -0,0 +1,11 @@
+# MODULES :-
+# --> module is nothing but collection of variables,classes and functions is called modules
+# --> each and every .py extension is called module
+# --> modules are two types,they are 1 customized module, 2.inbuilt module
+
+# customized modules :- (keyword: import from as)
+
+
+# InBuilt Modules :-
+
+# math,datetime,random,fonktools
