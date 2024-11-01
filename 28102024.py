1aee3adcb9559b5b584ebcab48ea66a73653f792
sreerampradeep553 <sreerampradeep0553@gmail.com>
2024-11-01 08:07:54 +0530

    task added by pradeep

---
+++ 28102024.py
@@ -0,0 +1,27 @@
+# OOPS (Object Oriented Programming Service)
+
+# Oops is nothing but to create classes and Objects in real time projects 
+
+# CLASS :- class is nothing but model or blue print
+# OBJECT:- object is nothing but it is an real time entity..by using class we can create number of objects
+
+# In real time applications of oops is 
+# Banking, gaming,mobile applications etc...
+
+
+class metroblueprint:
+    def display(self,sname):
+        print(sname,"metroblueprint")
+
+m=metroblueprint() # here 'm' is object reference variable
+m.display(input("enter metro name:"))
+m.display(input("enter metroname:"))
+
+
+## IN OPPS CONCEPT WE HAVE FOUR PILLERS
+
+# --> 1.ENCAPSULATION
+# --> 2.ABSTRACTION
+# --> 3.POLYMORPHISM
+# --> 4.INHERITANCE
+
